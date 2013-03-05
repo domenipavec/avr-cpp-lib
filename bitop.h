acdd095c34752d989f1c0b4be857b5ad4dd72241
Domen Ipavec <domen.ipavec@z-v.si>
2013-03-05 10:54:03 +0100

    Documented macros in bitop.h

--- bitop.h
+++ bitop.h
@@ -1,4 +1,4 @@
-/* File: bitop.h
+/*! \file bitop.h
  * Contains macros for simple bit operations.
  */
 /* Copyright (c) 2012-2013 Domen Ipavec (domen.ipavec@z-v.si)
@@ -26,15 +26,84 @@
 #ifndef BITTOP_H
 #define BITTOP_H
 
-#define BIT(x) (1 << (x)) 
-#define SETBITS(x,y) ((x) |= (y)) 
-#define CLEARBITS(x,y) ((x) &= (~(y))) 
-#define SETBIT(x,y) SETBITS((x), (BIT((y)))) 
-#define CLEARBIT(x,y) CLEARBITS((x), (BIT((y)))) 
-#define BITSET(x,y) ((x) & (BIT(y))) 
-#define BITCLEAR(x,y) !BITSET((x), (y)) 
-#define BITSSET(x,y) (((x) & (y)) == (y)) 
-#define BITSCLEAR(x,y) (((x) & (y)) == 0) 
-#define BITVAL(x,y) (((x)>>(y)) & 1) 
+/*!
+ * \defgroup Bitop 
+ * \brief A few useful macros for setting, clearing, checking, ... bit(s).
+ * 
+ * Bit count starts at 0 for the least significant bit.
+ * @{
+ */
+
+/*!
+ * Returns binary representation of n-th bit.
+ * Example: 2 --> 0100b
+ * \param n Number of bit.
+ */
+#define BIT(n) (1 << (n))
+
+/*!
+ * Sets bits in x, that are set in y.
+ * \param x Variable.
+ * \param y Binary representation of bits to set in x.
+ */
+#define SETBITS(x,y) ((x) |= (y))
+
+/*!
+ * Clear bits in x, that are set in y.
+ * \param x Variable.
+ * \param y Binary representation of bits to clear in x.
+ */ 
+#define CLEARBITS(x,y) ((x) &= (~(y)))
+
+/*!
+ * Set n-th bit in x.
+ * \param x Variable.
+ * \param n Number of bit to set.
+ */
+#define SETBIT(x,n) (SETBITS((x), BIT((n))))
+
+/*!
+ * Clear n-th bit in x.
+ * \param x Variable.
+ * \param n Number of bit to clear.
+ */
+#define CLEARBIT(x,n) (CLEARBITS((x), BIT((n))))
+
+/*!
+ * Check if n-th bit in x is set.
+ * \param x Variable.
+ * \param n Number of bit to check.
+ */ 
+#define BITSET(x,n) ((x) & BIT((n)))
+
+/*!
+ * Check if n-th bit in x is clear.
+ * \param x Variable.
+ * \param n Number of bit to check.
+ */ 
+#define BITCLEAR(x,y) (!BITSET((x), (y)))
+
+/*!
+ * Check if bits set in y are also set in x.
+ * \param x Variable.
+ * \param y Binary representation of bits to check.
+ */
+#define BITSSET(x,y) (((x) & (y)) == (y))
+
+/*!
+ * Check if bits set in y are clear in x.
+ * \param x Variable.
+ * \param y Binary representation of bits to check.
+ */
+#define BITSCLEAR(x,y) (((x) & (y)) == 0)
+
+/*!
+ * Get value of n-th bit (0 or 1).
+ * \param x Variable.
+ * \param n Number of bit.
+ */
+#define BITVAL(x,n) (((x)>>(n)) & 1)
+
+/*! }@ */
 
 #endif
